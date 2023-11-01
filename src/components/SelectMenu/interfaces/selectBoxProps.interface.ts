45ca7525072d880599ccad4c30314ff08b3b5057
Andy Nguyen <andy.nguyen.goldenowl@gmail.com>
2023-11-01 22:58:27 +0700

    fix: move optional property interface to end

--- src/components/SelectMenu/interfaces/selectBoxProps.interface.ts
+++ src/components/SelectMenu/interfaces/selectBoxProps.interface.ts
@@ -1,8 +1,8 @@
 import type React from "react";
 
 interface SelectBoxProps {
-  className?: string;
   options: string[];
+  className?: string;
   background?: string;
   textColor?: string;
   onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
