824d85b1773e4e678a01e6e099ef7c6dfb9a699a
Andy Nguyen <andy.nguyen.goldenowl@gmail.com>
2023-11-01 09:05:10 +0700

    refactor: create utils functions

---
+++ src/utils/functions.ts
@@ -0,0 +1,10 @@
+export const getRandomInt = (min: number, max: number): number => {
+  if (max < min) {
+    const temp: number = max;
+    max = min;
+    min = temp;
+  }
+  const randomNum = Math.floor(Math.random() * (max - min) + min);
+
+  return randomNum;
+};
