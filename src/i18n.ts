459b2c95ab38da322469b0b82dde647b7d823990
Andy Nguyen <andy.nguyen.goldenowl@gmail.com>
2023-11-02 13:14:47 +0700

    fix: use LangEnum in i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -10,7 +10,7 @@ void i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: "en",
+    fallbackLng: LangEnum.en,
     lng: localStorage.getItem("i18nextLng") ?? LangEnum.en,
     debug: true,
 
